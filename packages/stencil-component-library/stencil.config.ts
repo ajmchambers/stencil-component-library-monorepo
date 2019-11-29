9945e34e5040b41848a9e40f0fa71f1f8fdb0117
Alan Chambers <ajmchambers@gmail.com>
2019-11-29 13:51:58 +0000

    Update stencil.config.ts

--- packages/stencil-component-library/stencil.config.ts
+++ packages/stencil-component-library/stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'initial-stencil-component',
+  namespace: 'stencil-component-library',
   outputTargets: [
     {
       type: 'dist',
